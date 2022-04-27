eb4a746d2c40e3e3bff49dcc8d7a455548f5a186
Brian Martin <martinbd1@gmail.com>
2022-04-27 16:40:07 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,31 +1,44 @@
 // src/components/movie-view/movie-view.jsx
 
 import React from "react";
+import { Button, Container, Col, Row, Card } from "react-bootstrap";
 
 export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
 
+    if (!movies) return null;
+
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}>
-          Back
-        </button>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text> {movie.Description}</Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    onBackClick(null);
+                  }}>
+                  Back
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
