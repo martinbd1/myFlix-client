bb54d7ccb62001205b1420c44ecc17ff9fa33d16
Brian Martin <martinbd1@gmail.com>
2022-05-03 22:59:07 -0400

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 // src/components/login-view/login-view.jsx
 
 import React, { useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { Form, Button, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
 
@@ -9,13 +10,50 @@ import "./login-view.scss";
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
+  //vallidate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 3) {
+      setUsernameErr("Username must be 3 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 8) {
+      setUsernameErr("Password must be 8 characters long");
+      isReq = false;
+    }
+
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    const isReq = validate();
+    if (isReq) {
+      axios
+        /* Send a request to the server for authentication */
+        .post("https://myflixapp1987.herokuapp.com/login", {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log("no such user");
+        });
+    }
   };
 
   return (
@@ -27,22 +65,25 @@ export function LoginView(props) {
               <Card.Body>
                 <Card.Title>Please Login</Card.Title>
                 <Form>
-                  <Form.Group>
+                  <Form.Group controlId="formUsername" className="reg-form-inputs">
                     <Form.Label>Username:</Form.Label>
                     <Form.Control
                       type="text"
-                      onChange={(e) => setUsername(e.target.value)}
                       placeholder="Enter your username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                     />
+                    {usernameErr && <p>{usernameErr}</p>}
                   </Form.Group>
 
-                  <Form.Group>
+                  <Form.Group controlId="formPassword" className="reg-form-inputs">
                     <Form.Label>Password:</Form.Label>
                     <Form.Control
                       type="password"
-                      onChange={(e) => setPassword(e.target.value)}
                       placeholder="Enter your password"
+                      onChange={(e) => setPassword(e.target.value)}
                     />
+                    {passwordErr && <p>{passwordErr}</p>}
                   </Form.Group>
 
                   <Button variant="primary" type="submit" onClick={handleSubmit}>
