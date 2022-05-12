763e9ed5f164e1c6ded5937ccfc854ba85e921f1
Brian Martin <martinbd1@gmail.com>
2022-05-12 19:01:32 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,8 +3,12 @@
 import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import { Button, Container, Col, Row, Card } from "react-bootstrap";
+import { Button, Container, Col, Row, Card, CardGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import FavoriteToggle from "./favorite-toggle";
+import StarFill from "../../../img/star-filled.png";
+import StarEmpty from "../../../img/star-empty.png";
 
 import "./movie-view.scss";
 
@@ -13,98 +17,105 @@ export class MovieView extends React.Component {
     super();
     // Initial state is set to null
     this.state = {
-      Username: null,
-      Password: null,
-      Email: null,
-      Birthday: null,
       FavoriteMovies: [],
+      isFavorite: false,
     };
   }
 
-  componentDidMount() {
-    const accessToken = localStorage.getItem("token");
-    this.getUser(accessToken);
-  }
-
-  getUser(token) {
-    const user = localStorage.getItem("user");
-
-    axios
-      .get(`https://myflixapp1987.herokuapp.com/users/${user}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        // Assign the result to the state
-        this.setState({
-          Username: response.data.Username,
-          Password: response.data.Password,
-          Email: response.data.Email,
-          Birthday: response.data.Birthday,
-          FavoriteMovies: response.data.FavoriteMovies,
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  onAddFavorite = () => {
+  onAddFavorite = (e, movie) => {
+    //e.preventDefault();
     const user = localStorage.getItem("user");
     const token = localStorage.getItem("token");
     const id = this.props.movie._id;
     ///users/:Username/movies/:MovieID
     axios
-      .post(`https://myflixapp1987.herokuapp.com/users/${user}/movies/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
+      .post(
+        `https://myflixapp1987.herokuapp.com/users/${user}/movies/${id}`,
+        {
+          FavoriteMovies: this.state.FavoriteMovies,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
       .then((response) => {
         console.log(response);
-        this.componentDidMount;
+        this.setState({ FavoriteMovies: response.data.FavoriteMovies });
         alert("Movie has been added to Favorites");
+        this.componentDidMount;
       })
       .catch(function (error) {
         console.log(error);
       });
   };
 
-  onRemoveFavorite = () => {
-    const user = localStorage.getItem("user");
-    const token = localStorage.getItem("token");
-    const id = this.props.movie._id;
-    ///users/:Username/movies/:MovieID
-    axios
-      .delete(`https://myflixapp1987.herokuapp.com/users/${user}/movies/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        console.log(response);
-        this.componentDidMount;
-        alert("Movie has been removed form Favorites");
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+  // onRemoveFavorite = (e, movie) => {
+  //   //e.preventDefault();
+  //   const user = localStorage.getItem("user");
+  //   const token = localStorage.getItem("token");
+  //   const id = this.props.movie._id;
+  //   ///users/:Username/movies/:MovieID
+  //   axios
+  //     .delete(`https://myflixapp1987.herokuapp.com/users/${user}/movies/${id}`, {
+  //       headers: { Authorization: `Bearer ${token}` },
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //       alert("Movie has been removed form Favorites");
+  //       this.componentDidMount;
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
+
+  // checkFavorite = () => {
+  //   const id = this.props.movie._id;
+  //   if (favorites.includes(id) !== 0) {
+  //     this.setState({ isFavorite: true });
+  //     <img
+  //       onClick={() => {
+  //         this.onRemoveFavorite();
+  //       }}
+  //       src={StarFill}></img>;
+  //   } else {
+  //     this.setState({ isFavorite: false });
+  //     <img
+  //       onClick={() => {
+  //         this.onAddFavorite();
+  //       }}
+  //       src={StarEmpty}></img>;
+  //   }
+  // };
 
   render() {
-    const { user, movie, onBackClick } = this.props;
+    const { movie, user, getFav, onBackClick } = this.props;
+    const { isFavorite } = this.state;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
-              <Card.Body>
-                <Card.Title>{movie.Title}</Card.Title>
+      <Row>
+        <Col className="col-8">
+          <Card className="movie-view-card">
+            <Card.Body>
+              <Card.Title className="title-row">
+                <h1>{movie.Title} </h1>
+                {/* <FavoriteToggle isFavorite={isFavorite} movie={movie} user={user} /> */}
+              </Card.Title>
 
-                <Card.Text> {movie.Description}</Card.Text>
-                <Link to={`/directors/${movie.Director.Name}`}>
-                  <Button variant="link">Director</Button>
-                </Link>
-                <Link to={`/genres/${movie.Genre.Name}`}>
-                  <Button variant="link">Genre</Button>
-                </Link>
+              <Card.Text>{movie.Description}</Card.Text>
+              <div className="director-row">
+                <p>
+                  <b>Director: </b>
+                  <Link to={`/directors/${movie.Director.Name}`}>{movie.Director.Name}</Link>
+                </p>
+              </div>
+              <div className="genre-row">
+                <p>
+                  <b>Genre: </b>
+                  <Link to={`/genres/${movie.Genre.Name}`}>{movie.Genre.Name}</Link>
+                </p>
+              </div>
+              <div>
                 <Button
                   variant="warning"
                   onClick={() => {
@@ -119,20 +130,45 @@ export class MovieView extends React.Component {
                   }}>
                   Back
                 </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card className="movie-view-card">
+            <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
 
 MovieView.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired,
-  }).isRequired,
+  movie: PropTypes.arrayOf(
+    PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+      }).isRequired,
+      Director: PropTypes.shape({
+        Bio: PropTypes.string.isRequired,
+        Birth: PropTypes.string.isRequired,
+        Death: PropTypes.string,
+        Name: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
+
+let mapStateToProps = (state) => {
+  const { favorites } = state;
+  return { favorites };
+};
+
+export default connect(mapStateToProps)(MovieView);
